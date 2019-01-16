ed964aee1f1454f1e49ce1a58cc4dd7d75d8138d
Vaibhav Jain <vjain83@csc.com>
2019-01-16 16:41:46 +0530

    update

--- Assignment - Stock Market Analysis - MySQL/Solutions/Solution4.sql
+++
@@ -1,9 +0,0 @@
--- Function for Checking Signal
-
-create function stock (dd Date)
-returns char(10) deterministic
-
-return (select `Signal` from bajaj2 where date = dd);
-
-
-select stock('2017-05-03') ;
